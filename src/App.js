1f6c5c30c368e9ad1e1e0cc848fe31fccfc19ba2
Amy Gardiner <amymgardiner@gmail.com>
2023-06-01 14:01:04 -0500

    starting on outline of homepage

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import { Route, Routes } from "react-router-dom";
 import Home from './components/Home'
 
 function App() {
-  return (
-    <Routes>
-		  <Route path='/' element={<Home />}/>
-	  </Routes>
-  );
+	return (
+		<Routes>
+			<Route path='/' element={<Home />}/>
+		</Routes>
+	);
 }
 
 export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Home = () => {
+	return (
+
+		<main>
+			<div className="home--hero">
+			</div>
+
+			<div className="home--feature">
+			</div>
+
+			<div className="home--products">
+			</div>
+
+			<div className="home--contact">
+			</div>
+		</main>
+	)
+}
+
+export default Home
