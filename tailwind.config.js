bf00e2711966d53b7eb489d0675218e18ab3e68d
Amy Gardiner <amymgardiner@gmail.com>
2023-06-01 14:28:41 -0500

    changed icon from react to fake dunder mifflin

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,22 @@ module.exports = {
     ],
     theme: {
       extend: {},
+      screens: {
+        'sm': {'max': '640px'},
+        // => @media (max-width: 640px) { ... }
+  
+        'md': {'max': '768px'},
+        // => @media (max-width: 768px) { ... }
+  
+        'lg': {'max': '1024px'},
+        // => @media (max-width: 1024px) { ... }
+  
+        'xl': {'max': '1280px'},
+        // => @media (max-width: 1280px) { ... }
+  
+        '2xl': {'max': '1536px'},
+        // => @media (max-width: 1536px) { ... }
+      },
     },
     plugins: [],
   }
