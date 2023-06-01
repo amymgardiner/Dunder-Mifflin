137ffd3bec99b5b31d97cc2bfbb3f15d45bd17a3
Amy Gardiner <amymgardiner@gmail.com>
2023-06-01 13:48:07 -0500

    installed react router dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Route, Routes } from "react-router-dom";
+import Home from './components/Home'
 
 function App() {
   return (
-    <div>hi</div>
+    <Routes>
+		  <Route path='/' element={<Home />}/>
+	  </Routes>
   );
 }
 
