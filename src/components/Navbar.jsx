aa7468e26788e98c72836e8f61c076121930c089
Amy Gardiner <amymgardiner@gmail.com>
2023-07-31 16:25:35 -0500

    starting on dropdown menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,10 +18,15 @@ const Navbar = () => {
 				<div>
 					<h2 className="font-serif text-2xl tracking-wider text-white uppercase md:text-xl hvr-grow">Partnerships</h2>
 				</div>
-				<div className="hvr-grow">
-					<div className="flex items-center gap-2">
-						<h2 className="font-serif text-2xl tracking-wider text-white uppercase md:text-xl">Shop </h2>
-						<p className="text-xl text-white md:text-lg"><BiDownArrow/></p>
+				<div className="dropdown">
+					<div className="hvr-grow">
+						<div className="flex items-center gap-2">
+							<h2 className="font-serif text-2xl tracking-wider text-white uppercase md:text-xl">Shop </h2>
+							<p className="text-xl text-white md:text-lg"><BiDownArrow/></p>
+						</div>
+					</div>
+					<div className="dropdown-content">
+						hi
 					</div>
 				</div>
 			</div>
