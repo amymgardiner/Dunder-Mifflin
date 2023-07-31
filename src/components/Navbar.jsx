2b4a0b79b131fb95ff63c38b1a2e874bbf95c7e9
Amy Gardiner <amymgardiner@gmail.com>
2023-07-31 15:19:32 -0500

    starting on navbar media queries

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,24 +4,24 @@ import Logo from '../assets/img/Dunder_Mifflin,_Inc.svg'
 
 const Navbar = () => {
 	return (
-		<nav className="flex items-center gap-16 main--navbar bg-navy-blue">
+		<nav className="flex items-center gap-16 md:gap-8 main--navbar bg-navy-blue wrapper">
 			<div>
 				<img src={Logo} alt="Hammermill Copy Plus Copy Paper" className="w-36"></img>
 			</div>
-			<div className="flex gap-16">
+			<div className="flex gap-16 lg:gap-10 md:gap-8">
 				<div>
-					<h2 className="font-serif text-2xl tracking-wider text-white uppercase hvr-grow">About</h2>
+					<h2 className="font-serif text-2xl tracking-wider text-white uppercase md:text-xl hvr-grow">About</h2>
 				</div>
 				<div>
-					<h2 className="font-serif text-2xl tracking-wider text-white uppercase hvr-grow">Our Team</h2>
+					<h2 className="font-serif text-2xl tracking-wider text-white uppercase md:text-xl hvr-grow">Our Team</h2>
 				</div>
 				<div>
-					<h2 className="font-serif text-2xl tracking-wider text-white uppercase hvr-grow">Partnerships</h2>
+					<h2 className="font-serif text-2xl tracking-wider text-white uppercase md:text-xl hvr-grow">Partnerships</h2>
 				</div>
 				<div className="hvr-grow">
 					<div className="flex items-center gap-2">
-						<h2 className="font-serif text-2xl tracking-wider text-white uppercase">Shop </h2>
-						<p className="text-xl text-white"><BiDownArrow/></p>
+						<h2 className="font-serif text-2xl tracking-wider text-white uppercase md:text-xl">Shop </h2>
+						<p className="text-xl text-white md:text-lg"><BiDownArrow/></p>
 					</div>
 				</div>
 			</div>
