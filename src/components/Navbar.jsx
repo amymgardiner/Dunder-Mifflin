58242f9408fe544dbc8f15b4d7c69aa51acf676c
Amy Gardiner <amymgardiner@gmail.com>
2023-08-09 16:12:02 -0500

    working on mobile screen sizes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,11 +11,11 @@ const Navbar = () => {
   	};
 
 	return (
-		<nav className="flex items-center gap-16 md:gap-8 main--navbar bg-navy-blue wrapper">
+		<nav className="flex items-center gap-16 md:gap-2 main--navbar bg-navy-blue wrapper md:flex-col md:pb-4">
 			<div>
 				<img src={Logo} alt="Hammermill Copy Plus Copy Paper" className="w-36"></img>
 			</div>
-			<div className="flex gap-16 lg:gap-10 md:gap-8">
+			<div className="flex gap-16 lg:gap-10 md:!gap-8 main--navbar-links">
 				<div>
 					<h2 className="font-serif text-2xl tracking-wider text-white uppercase md:text-xl hvr-grow">About</h2>
 				</div>
@@ -34,9 +34,9 @@ const Navbar = () => {
 					</div>
 					<div className={`dropdown-content ${isDropdownVisible ? 'visible' : 'hidden'}`}>
 						<div className="flex dropdown-content--text">
-							<p className="mt-2 font-serif text-xl tracking-wider hvr-grow">Paper</p>
-							<p className="mt-2 font-serif text-xl tracking-wider hvr-grow">Office Supplies</p>
-							<p className="mt-2 font-serif text-xl tracking-wider hvr-grow">Printers & Copiers</p>
+							<p className="mt-2 font-serif text-xl tracking-wider hvr-grow md:text-lg">Paper</p>
+							<p className="mt-2 font-serif text-xl tracking-wider hvr-grow md:text-lg">Office Supplies</p>
+							<p className="mt-2 font-serif text-xl tracking-wider hvr-grow md:text-lg">Printers & Copiers</p>
 						</div>
 					</div>
 				</div>
