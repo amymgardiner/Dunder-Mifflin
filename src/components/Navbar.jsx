465e25f6718a5d13a2dd380eb12e668ab5cb260a
Amy Gardiner <amymgardiner@gmail.com>
2023-08-09 15:55:58 -0500

    working on navbar, made dropdown work on click and did styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { BiDownArrow } from "react-icons/bi";
 import Logo from '../assets/img/Dunder_Mifflin,_Inc.svg'
 
 const Navbar = () => {
+
+	const [isDropdownVisible, setIsDropdownVisible] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownVisible(!isDropdownVisible);
+  	};
+
 	return (
 		<nav className="flex items-center gap-16 md:gap-8 main--navbar bg-navy-blue wrapper">
 			<div>
@@ -18,15 +25,19 @@ const Navbar = () => {
 				<div>
 					<h2 className="font-serif text-2xl tracking-wider text-white uppercase md:text-xl hvr-grow">Partnerships</h2>
 				</div>
-				<div className="dropdown">
+				<div className="dropdown" onClick={toggleDropdown}>
 					<div className="hvr-grow">
 						<div className="flex items-center gap-2">
 							<h2 className="font-serif text-2xl tracking-wider text-white uppercase md:text-xl">Shop </h2>
 							<p className="text-xl text-white md:text-lg"><BiDownArrow/></p>
 						</div>
 					</div>
-					<div className="dropdown-content">
-						hi
+					<div className={`dropdown-content ${isDropdownVisible ? 'visible' : 'hidden'}`}>
+						<div className="flex dropdown-content--text">
+							<p className="mt-2 font-serif text-xl tracking-wider hvr-grow">Paper</p>
+							<p className="mt-2 font-serif text-xl tracking-wider hvr-grow">Office Supplies</p>
+							<p className="mt-2 font-serif text-xl tracking-wider hvr-grow">Printers & Copiers</p>
+						</div>
 					</div>
 				</div>
 			</div>
