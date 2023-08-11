c73af010d20cef7a3178a1bd9b7fd4f84f196e1d
Amy Gardiner <amymgardiner@gmail.com>
2023-08-11 16:02:31 -0500

    finished partnerships page

--- src/components/Partnerships.jsx
+++ src/components/Partnerships.jsx
@@ -4,6 +4,9 @@ import React from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
 
+// Tools
+import { TbPointFilled } from "react-icons/tb";
+
 // Images
 import One from '../assets/img/9023668_number_circle_one_fill_icon.svg'
 import Two from '../assets/img/9023576_number_circle_two_fill_icon.svg'
@@ -81,30 +84,30 @@ const Partnerships = () => {
 			</section>
 
 			{/* community partners */}
-			<section className="wrapper">
-				<div>
-					<h2>Dunder Mifflin: Where partnership meets purpose.</h2>
-					<p>At Dunder Mifflin, we believe that great partnerships build strong communities and a sustainable future. Our commitment to quality, innovation, and responsible business practices extends beyond the walls of our offices. We're proud to collaborate with local businesses, support eco-friendly initiatives, and make a positive impact through volunteerism and charitable giving.</p>
+			<section className="py-20 wrapper partnerships--community-section">
+				<div className="px-16 pb-4 text-center md:px-0">
+					<h2 className="font-sans text-4xl font-semibold md:text-3xl">Dunder Mifflin: Where partnership meets purpose.</h2>
+					<p className="pt-4 text-2xl leading-loose font-default md:text-xl">At Dunder Mifflin, we believe that great partnerships build strong communities and a sustainable future. Our commitment to quality, innovation, and responsible business practices extends beyond the walls of our offices. We're proud to collaborate with local businesses, support eco-friendly initiatives, and make a positive impact through volunteerism and charitable giving.</p>
 				</div>
-				<div>
-					<h2>Local Business Collaborations</h2>
-					<p>We're honored to work hand-in-hand with some of the finest local businesses, just as we're known for in the Scranton area. From "Alfredo's Pizza Cafe," where we celebrate great food, to "Poor Richard's Pub," where good times are always on tap, these partnerships bring us closer to the heart of Scranton.</p>
+				<div className="px-16 pt-8 md:px-0">
+					<h3 className="flex font-sans text-3xl font-semibold md:text-2xl"><TbPointFilled/>Local Business Collaborations</h3>
+					<p className="px-8 mt-4 text-xl leading-loose md:px-4 md:text-lg font-default">We're honored to work hand-in-hand with some of the finest local businesses, just as we're known for in the Scranton area. From "Alfredo's Pizza Cafe," where we celebrate great food, to "Poor Richard's Pub," where good times are always on tap, these partnerships bring us closer to the heart of Scranton.</p>
 				</div>
-				<div>
-					<h2>A Sustainable Focus: Recycling Paper Partnerships</h2>
-					<p>Dunder Mifflin is more than just a paper company; we're a steward of the environment. We proudly collaborate with recycling champions like "Hooters," who have demonstrated a commitment to sustainability through their innovative recycling programs. Together, we're reducing our carbon footprint, one sheet of paper at a time.</p>
+				<div className="px-16 pt-8 md:px-0">
+					<h3 className="flex font-sans text-3xl font-semibold md:text-2xl"><TbPointFilled/>A Sustainable Focus: Recycling Paper Partnerships</h3>
+					<p className="px-8 mt-4 text-xl leading-loose md:text-lg font-default md:px-4">Dunder Mifflin is more than just a paper company; we're a steward of the environment. We proudly collaborate with recycling champions like "Hooters," who have demonstrated a commitment to sustainability through their innovative recycling programs. Together, we're reducing our carbon footprint, one sheet of paper at a time.</p>
 				</div>
-				<div>
-					<h2>Community Support: Volunteering and Non-Profits</h2>
-					<p>Giving back is a fundamental part of the Dunder Mifflin ethos. We actively engage with local non-profits such as "The Children's Hospital" and "The Michael Scott Foundation," dedicated to making a difference in the lives of children and families. Our team is also passionate about volunteering, whether it's cleaning up the local park or helping out at the "Golden Ticket Charity."</p>
+				<div className="px-16 pt-8 md:px-0">
+					<h3 className="flex font-sans text-3xl font-semibold md:text-2xl"><TbPointFilled/>Community Support: Volunteering and Non-Profits</h3>
+					<p className="px-8 mt-4 text-xl leading-loose md:text-lg font-default md:px-4">Giving back is a fundamental part of the Dunder Mifflin ethos. We actively engage with local non-profits such as "The Children's Hospital" and "The Michael Scott Foundation," dedicated to making a difference in the lives of children and families. Our team is also passionate about volunteering, whether it's cleaning up the local park or helping out at the "Golden Ticket Charity."</p>
 				</div>
-				<div>
-					<h2>Innovative Collaborations</h2>
-					<p>We're always seeking new and exciting partnerships to enhance our products and services. Whether it's joining forces with technology leaders to optimize our office solutions or partnering with educational institutions to empower the next generation, Dunder Mifflin remains at the forefront of innovation.</p>
+				<div className="px-16 pt-8 md:px-0">
+					<h3 className="flex font-sans text-3xl font-semibold md:text-2xl"><TbPointFilled/>Innovative Collaborations</h3>
+					<p className="px-8 mt-4 text-xl leading-loose md:text-lg font-default md:px-4">We're always seeking new and exciting partnerships to enhance our products and services. Whether it's joining forces with technology leaders to optimize our office solutions or partnering with educational institutions to empower the next generation, Dunder Mifflin remains at the forefront of innovation.</p>
 				</div>
-				<div>
-					<h2>Join Us in Building a Brighter Future</h2>
-					<p>As we continue to expand our network of partners, we invite you to be a part of our journey. Together, we can achieve remarkable things, support our community, and work towards a more sustainable and prosperous future. Contact us today to explore partnership opportunities that align with our values and your business objectives.</p>
+				<div className="px-16 pt-8 md:px-0">
+					<h3 className="flex font-sans text-3xl font-semibold md:text-2xl"><TbPointFilled/>Join Us in Building a Brighter Future</h3>
+					<p className="px-8 mt-4 text-xl leading-loose md:text-lg font-default md:px-4">As we continue to expand our network of partners, we invite you to be a part of our journey. Together, we can achieve remarkable things, support our community, and work towards a more sustainable and prosperous future. Contact us today to explore partnership opportunities that align with our values and your business objectives.</p>
 				</div>
 			</section>
 			
